415834b8458707070aafc13deb885bfc7c67de1f
Adrian Kahali <adrian.kahali@hotmail.com>
2026-04-09 11:30:26 -0400

    setup shadcn

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -9,8 +9,6 @@ export const siteConfig = {
   name: 'Docs',
   logo: '/logo/light.svg',
   logoDark: '/logo/dark.svg',
-  primaryColor: '#5054A8',
-  darkModePrimaryColor: '#7B7FD1',
   navbar: {
     links: [{ label: 'Dashboard', href: 'https://app.botpress.cloud', type: 'button', external: true }] as NavbarLink[],
   },
