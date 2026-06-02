97139b13e64eecdf8386b3996839c31d66351f20
Janelle Tam <janelletam.work@gmail.com>
2026-06-02 12:35:38 -0400

    refactor(docs): update icons to use lucide astro icons throughout project, also add type checking for the icons in astro components

--- src/lib/icon-utils.ts
+++ src/lib/icon-utils.ts
@@ -6,10 +6,6 @@ export function toPascalCase(name: string): string {
     .join('')
 }
 
-export function toLucideKebab(name: string): string {
-  return name.replace(/([a-z])([A-Z0-9])/g, '$1-$2').toLowerCase()
-}
-
 export function slugify(text: string): string {
   return text
     .toLowerCase()
